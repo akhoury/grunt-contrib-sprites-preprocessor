68aa1278c4e95a5026314c9ae53e03a51ae066ad
Aziz Khoury <bentael@gmail.com>
2014-09-09 13:58:15 -0400

    working version

---
+++ README.md
@@ -0,0 +1,61 @@
+### grunt-contrib-sprites-preprocessor
+
+A grunt wrapper around https://github.com/madebysource/sprites-preprocessor
+
+The only sprites generator that handles CSS replacement well enough I found.
+
+### usage
+
+```javascript
+
+    grunt.initConfig({
+    	'sprite-preprocessor': {
+    		options: {
+				// generated img name, defaults to 'sprite.png'
+				// maps to https://github.com/madebysource/sprites-preprocessor#name
+				spriteFileName: 'sprite.png',
+
+				// where all the images for the src CSS files live, defaults to 'src/assets'
+				// maps to https://github.com/madebysource/sprites-preprocessor#path
+				srcImagesDir: 'src/assets',
+
+				// a mutual prefix to all the urls in the CSS content
+				// maps to https://github.com/madebysource/sprites-preprocessor#prefix
+				cssUrlsPrefixedWith: 'assets/',
+
+				// destination directory of the sprite, if 'spriteDestDir' is not specified it defaults to the value of 'srcImagesDir'
+				spriteDestDir: 'dist/assets'
+
+				// few general grunt task options
+
+				separator: '', // in case you have more than 1 src css file,
+				filter: 'isFile' // or a function
+				// see http://gruntjs.com/configuring-tasks#files
+            },
+    		files: {
+    			'dest/style.with.sprites.css': ['src/style.css' /* you can add more, they willl just be concatenated */]
+    		}
+    	}
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-sprites-preprocessor");
+
+```
+
+```bash
+
+npm install grunt-contrib-sprites-preprocessor
+grunt sprites-preprocessor
+
+```
+
+### requirements
+
+whatever [spritesmith](https://github.com/Ensighten/spritesmith) requires, I had to
+
+```
+sudo npm install -g phantomjs
+
+# and
+export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var sprites = require('sprites-preprocessor');
+var async = require('async');
+
+module.exports = function(grunt) {
+    grunt.registerMultiTask('sprites-preprocessor', 'Generate sprites and accordingly edited CSS', function() {
+
+        var taskOptions = this.options({
+            spriteFileName: 'sprite.png',
+            spriteDestDir: '',
+            srcImagesDir: '',
+            cssUrlsPrefixedWith: ''
+        });
+
+        var done = this.async();
+
+        this.files.forEach(function(file) {
+
+            var statsFns = ['isFile', 'isDirectory', 'isBlockDevice', 'isCharacterDevice', /* 'isSymbolicLink', */ 'isFIFO', 'isSocket'];
+            var filter = typeof taskOptions.filter == 'function' ?
+                taskOptions.filter : statsFns.indexOf(taskOptions.filter) ?
+                function(filepath) {
+                    return fs.statSync(filepath)[taskOptions.filter]();
+                } :
+                function() {
+                    return true;
+                };
+
+            var files = file.src.filter(function(filepath) {
+                if (!grunt.file.exists(filepath) && filter(filepath)) {
+                    grunt.log.warn('Source file ' + filepath + ' not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+
+            var count = 0;
+
+            var spritesOptions = {
+                path: taskOptions.srcImagesDir,
+                prefix: taskOptions.cssUrlsPrefixedWith
+            };
+
+            var nameParts = taskOptions.spriteFileName.split('.');
+            var extension = nameParts.pop();
+            var filename = nameParts.join('.');
+
+            var css = '';
+            async.eachLimit(files, 1, function(f, next) {
+                var oldCss = grunt.file.read(f);
+
+                spritesOptions.name = (!count ? taskOptions.spriteFileName : filename + '.' + count + '.' + extension);
+
+                sprites(spritesOptions, oldCss, function(err, newCss, image) {
+                    if (err) {
+                        return next(err);
+                    }
+                    css += (taskOptions.separator || '') + newCss;
+                    var imageFile = path.join(taskOptions.spriteDestDir || taskOptions.srcImagesDir, '/' + spritesOptions.name);
+                    fs.writeFile(imageFile, image, 'binary', function() {
+                        grunt.log.ok('Image file written: ' + imageFile);
+                        count++;
+                        next();
+                    });
+                });
+            }, function(err) {
+                if (err) {
+                    return done(err);
+                }
+
+                fs.writeFile(file.dest, css, {encoding: 'utf8'}, function(err) {
+                    if (err) {
+                        return done(err);
+                    }
+                    grunt.log.ok('CSS file written: ' + file.dest);
+                    done();
+                });
+            });
+        });
+    });
+};
